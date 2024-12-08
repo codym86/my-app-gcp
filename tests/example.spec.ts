57264e5fbd5d3c66cfcc6cbba64cab1cfc73830c
codym86 <codym86@gmail.com>
2024-12-09 01:52:42 +0530

    initial commit

---
+++ tests/example.spec.ts
@@ -0,0 +1,24 @@
+import { test, expect } from '@playwright/test';
+
+test('has title', async ({ page }) => {
+  await page.goto('/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle("Vite + React");
+
+    // Locate the h1 element
+    const h1 = await page.locator('h1');
+
+    // Assert that the h1 contains the expected text
+    await expect(h1).toHaveText('Hello All');
+});
+
+// test('get started link', async ({ page }) => {
+//   await page.goto('https://playwright.dev/');
+
+//   // Click the get started link.
+//   await page.getByRole('link', { name: 'Get started' }).click();
+
+//   // Expects page to have a heading with the name of Installation.
+//   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+// });
